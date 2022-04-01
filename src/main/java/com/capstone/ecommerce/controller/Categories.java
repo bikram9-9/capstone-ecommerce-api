1cfaccee6d78e9476dd6e434a3b92c8de0fd3bd5
Bikram Singh <bikramjeet.singh@hotmail.com>
2022-04-01 00:25:04 -0500

    intial changes

--- src/main/java/com/capstone/ecommerce/controller/Categories.java
+++ src/main/java/com/capstone/ecommerce/controller/Categories.java
@@ -3,20 +3,19 @@ package com.capstone.ecommerce.controller;
 import com.capstone.ecommerce.dao.CategoryRepository;
 import com.capstone.ecommerce.dao.ProductRepository;
 import com.capstone.ecommerce.model.Category;
-import com.capstone.ecommerce.model.Product;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.rmi.ServerException;
-import java.rmi.server.ServerCloneException;
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
+@CrossOrigin(origins ="*", allowedHeaders = "*")
 @RequestMapping("/category")
 public class Categories {
 
@@ -36,11 +35,23 @@ public class Categories {
     @PostMapping
     private ResponseEntity<Category> createCategory(@RequestBody Category category) throws ServerException {
         Category newCategory = categoryRepository.save(category);
+        System.out.println("THIS IS THE ID "+ newCategory.getId());
+        System.out.println("THIS IS THE ID "+ newCategory.getName());
 
         if(newCategory == null ){
             throw new ServerException("NEED USER CONTENT");
         }else{
-            return new ResponseEntity<>(newCategory, HttpStatus.CREATED);
+            return new ResponseEntity<>(newCategory, HttpStatus.OK);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    private ResponseEntity<Category> removeCategoryById(@PathVariable("id") Long categoryId) throws NoSuchFieldException {
+        if(categoryRepository.findById(categoryId).isPresent()){
+          categoryRepository.deleteById(categoryId);
+          return new ResponseEntity(HttpStatus.ACCEPTED);
+        }else{
+            throw new NoSuchFieldException("NO CATEGORY FOUND");
         }
     }
 
